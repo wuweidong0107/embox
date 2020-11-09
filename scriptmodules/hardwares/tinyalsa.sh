037ff54af5cef964a6e7f9531a90b53b3589ebaa
wuweidong <625769020@qq.com>
2020-11-09 19:40:22 +0800

    module: hardwares: add tinyalsa

---
+++ scriptmodules/hardwares/tinyalsa.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+emb_module_id="tinyalsa"
+emb_module_desc="tinyalsa - Tiny library to interface with ALSA in the Linux kernel"
+emb_module_help="/opt/embox/hardwares/tinyalsa"
+emb_module_section="hardware"
+
+function sources_tinyalsa() {
+    gitPullOrClone "$md_build" https://github.com/tinyalsa/tinyalsa
+}
+
+function build_tinyalsa() {
+    make clean
+    make
+}
+
+function install_tinyalsa() {
+    DESTDIR=${md_inst} PREFIX=/usr/local make install
+    make install
+    ldconfig
+}
