a1b545464be23cad1773334b13d931a09c776561
wuweidong <625769020@qq.com>
2020-11-13 07:12:14 +0800

    module: bsp: fa/rk3399/linux-4.4: build modules

--- scriptmodules/bsp/fa-rk3399-linux-4_4.sh
+++ scriptmodules/bsp/fa-rk3399-linux-4_4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 emb_module_id="fa-rk3399-linux-4_4"
-emb_module_desc="fa-rk3399-linux-4_4"
-emb_module_help="/opt/embox/bsp/fa-rk3399-linux-4_4"
+emb_module_desc="kernel for friendlyelec rk3399"
+emb_module_help="kernel-v4.4 for FriendlyElec rk3399 boards"
 emb_module_section="bsp"
 
 function depends_fa-rk3399-linux-4_4() {
@@ -17,9 +17,11 @@ function build_fa-rk3399-linux-4_4() {
     export PATH=${rootdir}/bsp/fa-toolchain/opt/FriendlyARM/toolchain/6.4-aarch64/bin/:$PATH
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux- nanopi4_linux_defconfig
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux- nanopi4-images -j16
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules -j16
 }
 
 function install_fa-rk3399-linux-4_4() {
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules_install INSTALL_MOD_PATH="$md_inst" -j16
     md_ret_files=(
         'kernel.img'
         'resource.img'
