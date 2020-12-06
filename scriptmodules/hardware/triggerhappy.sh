a72e885a8da31c30463f94a11b1683df0b18ccdf
wuweidong <625769020@qq.com>
2020-12-07 07:40:21 +0800

    module: hardware: add triggerhappy

---
+++ scriptmodules/hardware/triggerhappy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+emb_module_id="triggerhappy"
+emb_module_desc="triggerhappy - A lightweight hotkey daemon."
+emb_module_help="https://github.com/wertarbyte/triggerhappy"
+emb_module_section="hardware"
+
+function sources_triggerhappy() {
+    gitPullOrClone "$md_build" https://github.com/wertarbyte/triggerhappy
+}
+
+function build_triggerhappy() {
+    make -j16
+}
+
+function install_triggerhappy() {
+    make install DESTDIR=${md_inst}
+}
