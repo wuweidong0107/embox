66a818c372be144c05d822dd00ad24f77addefd0
wuweidong <625769020@qq.com>
2021-01-11 08:21:10 +0800

    module: bsp: add upgrade_tf for fa-rk3399-linux-4_19

--- scriptmodules/bsp/fa-rk3399-linux-4_19.sh
+++ scriptmodules/bsp/fa-rk3399-linux-4_19.sh
@@ -15,16 +15,69 @@ function sources_fa-rk3399-linux-4_19() {
 
 function build_fa-rk3399-linux-4_19() {
     export PATH=${rootdir}/bsp/fa-toolchain/opt/FriendlyARM/toolchain/6.4-aarch64/bin/:$PATH
+
+    touch .scmversion
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux- nanopi4_linux_defconfig
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux- nanopi4-images -j16
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules -j16
     make ARCH=arm64 CROSS_COMPILE=aarch64-linux- dtbs -j16
 }
 
+## @param target: install target (module|image)
 function install_fa-rk3399-linux-4_19() {
-    make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules_install INSTALL_MOD_PATH="$md_inst" -j16
-    md_ret_files=(
-        'kernel.img'
-        'resource.img'
-    )
+    local target=$1
+
+    export PATH=${rootdir}/bsp/fa-toolchain/opt/FriendlyARM/toolchain/6.4-aarch64/bin/:$PATH
+    case "${target}" in
+        "image")
+            md_ret_files=(
+                'kernel.img'
+                'resource.img'
+            )
+            ;;
+        "module")
+            make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules_install INSTALL_MOD_PATH="$md_inst" -j16
+            ;;
+        *)
+            make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules_install INSTALL_MOD_PATH="$md_inst" -j16
+            md_ret_files=(
+                'kernel.img'
+                'resource.img'
+            )
+            ;;
+    esac
+}
+
+function upgrade_tf_fa-rk3399-linux-4_19() {
+
+    local dev=$1
+    if ! embCheckBlkSize ${dev} 64000000; then
+        echo "Error: invalid block dev ${dev}"
+        exit 1
+    fi
+
+    # abandon "sd_update -d /dev/sdX -p parameter.txt" becasue
+    # sd_update will remake gpt partition table
+    local subdev
+    local label
+    declare -A kdata
+    kdata=(
+        ['resource']='5'
+        ['kernel']='6'
+        )
+    for idx in ${!kdata[@]}; do
+        subdev="${dev}${kdata[$idx]}"
+        label=$(blkid ${subdev} -o export | grep PARTLABEL | cut -d= -f2)
+        if [ "${label}" == "${idx}" ]; then
+            dd if=${md_inst}/${idx}.img of=${subdev}
+        else
+            echo "${subdev}'s label($label) != ${idx}"
+            exit 1
+        fi
+    done
+}
+
+function upgrade_usb_fa-rk3399-linux-4_19() {
+    # TODO
+    echo "unsupported yet"
 }
