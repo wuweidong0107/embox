b87d5657b92cc4f30bbac3be032967ff5140a41c
wuweidong <625769020@qq.com>
2021-02-05 11:48:51 +0800

    module: bsp: update fa-rk3328-linux-5.4

--- scriptmodules/bsp/fa-rk3328-linux-5_4.sh
+++ scriptmodules/bsp/fa-rk3328-linux-5_4.sh
@@ -7,6 +7,8 @@ emb_module_section="bsp"
 
 function depends_fa-rk3328-linux-5_4() {
     emb_callModule fa-toolchain
+    emb_callModule sd_update-bin
+    emb_callModule sd-fuse_rk3328
 }
 
 function sources_fa-rk3328-linux-5_4() {
@@ -28,10 +30,22 @@ function build_fa-rk3328-linux-5_4() {
     ${sdfuse_tools}/prebuilt/boot/logo.bmp ${sdfuse_tools}/prebuilt/boot/logo_kernel.bmp
 }
 
+function _install_module_fa-rk3328-linux-5_4() {
+    make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules_install INSTALL_MOD_PATH="$md_inst" -j16
+    unlink "$md_inst/lib/modules/${kver}/source"
+    unlink "$md_inst/lib/modules/${kver}/build"
+}
+
 ## @param target: install target (module|image)
 function install_fa-rk3328-linux-5_4() {
     local target=$1
 
+    [ "${target}" == "help" ] \
+        && echo "Usage: embox_packages.sh ${md_id} install <target>[image|module]" \
+        && return
+
+    export PATH=${rootdir}/bsp/fa-toolchain/opt/FriendlyARM/toolchain/6.4-aarch64/bin/:$PATH
+    local kver=$(make ARCH=arm64 CROSS_COMPILE=aarch64-linux- kernelrelease)
     cp ${scriptdir}/scriptmodules/${md_type}/fa-rk3328-linux-5_4/partmap.txt ${md_inst}
     case "${target}" in
         "image")
@@ -41,10 +55,10 @@ function install_fa-rk3328-linux-5_4() {
             )
             ;;
         "module")
-            make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules_install INSTALL_MOD_PATH="$md_inst" -j16
+            _install_module_fa-rk3328-linux-5_4
             ;;
         *)
-            make ARCH=arm64 CROSS_COMPILE=aarch64-linux- modules_install INSTALL_MOD_PATH="$md_inst" -j16
+            _install_module_fa-rk3328-linux-5_4
             md_ret_files=(
                 'kernel.img'
                 'resource.img'
@@ -55,10 +69,17 @@ function install_fa-rk3328-linux-5_4() {
 
 function upgrade_tf_fa-rk3328-linux-5_4() {
     local dev="$1"
-    [[ -z "${dev}" ]] && echo "Error: miss tfcard device, e.g. /dev/sdX" && return
+
+    [ "${dev}" == "help" ] \
+        && echo "Usage: embox_packages.sh ${md_id} upgrade_tf /dev/sdX" \
+        && return
+
+    [[ -z "${dev}" ]] \
+        && echo "Usage: embox_packages.sh ${md_id} upgrade_tf /dev/sdX" \
+        && return
 
     local devname=$(basename ${dev})
-    local sdupdate="${md_build}/sd-fuse_rk3328/tools/sd_update"
+    local sdupdate="${scriptdir}/tmp/build/sd_update-bin/x86_64/sd_update"
     local partmap="${md_inst}/partmap.txt"
     local blksize=$(cat /sys/class/block/${devname}/size)
 
@@ -78,4 +99,40 @@ function upgrade_tf_fa-rk3328-linux-5_4() {
 function upgrade_usb_fa-rk3328-linux-5_4() {
     # TODO
     echo "unsupported yet"
+}
+
+function upgrade_ssh_fa-rk3328-linux-5_4() {
+    local ip="$1"
+
+    [ "${ip}" == "help" ] \
+        && echo "Usage: embox_packages.sh ${md_id} upgrade_ssh ip <target>[image|module]" \
+        && return
+
+    [[ -z "${ip}" ]] \
+        && echo "Usage: embox_packages.sh ${md_id} upgrade_ssh ip <target>[image|module]" \
+        && return
+
+    local target="$2"
+
+    local data=(
+        "${scriptdir}/tmp/build/sd_update-bin/aarch64/sd_update"
+        "${md_inst}/partmap.txt"
+        "${md_inst}/kernel.img"
+        "${md_inst}/resource.img"
+    )
+    local dest="root@${ip}:/root/"
+    case "${target}" in
+        "image")
+            scp ${data[*]} ${dest}
+            echo -e "\nRun command on RK3328:\n $ cd root\n $ ./sd_update -d /dev/mmcblkX -p partmap.txt"
+            ;;
+        "module")
+            scp -r ${md_inst}/lib root@${ip}:/
+            ;;
+        *)
+            scp -r ${md_inst}/lib root@${ip}:/
+            scp ${data[*]} ${dest}
+            echo -e "\nRun command on RK3328:\n $ cd root\n $ ./sd_update -d /dev/mmcblkX -p partmap.txt"
+            ;;
+    esac
 }
