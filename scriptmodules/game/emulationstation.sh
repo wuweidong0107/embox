4217df06eb7e7a486c37568ad76c1747cecbd057
wuweidong <625769020@qq.com>
2021-02-26 08:32:06 +0800

    module: game: update emulationstation

--- scriptmodules/game/emulationstation.sh
+++ scriptmodules/game/emulationstation.sh
@@ -6,5 +6,23 @@ emb_module_help="https://github.com/RetroPie/EmulationStation"
 emb_module_section="game"
 
 function sources_emulationstation() {
-    gitPullOrClone "$md_build" https://github.com/RetroPie/EmulationStation stable
+    gitPullOrClone "$md_build" https://github.com/RetroPie/EmulationStation stable "" 10000
+}
+
+function build_emulationstation() {
+    local params=(-DFREETYPE_INCLUDE_DIRS=/usr/include/freetype2/)
+    cmake . "${params[@]}"
+    make
+    md_ret_require="$md_build/emulationstation"
+}
+
+function install_emulationstation() {
+    md_ret_files=(
+        'CREDITS.md'
+        'emulationstation'
+        'emulationstation.sh'
+        'GAMELISTS.md'
+        'README.md'
+        'THEMES.md'
+    )
 }
