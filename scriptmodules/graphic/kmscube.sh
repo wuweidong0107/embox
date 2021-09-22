4569097a31d8e63cd737e6b4c52cef00e46be3c5
wuweidong <625769020@qq.com>
2021-09-22 15:36:34 +0800

    module: gfx: kmscube: auto maintain meson's version

--- scriptmodules/graphic/kmscube.sh
+++ scriptmodules/graphic/kmscube.sh
@@ -9,9 +9,20 @@ function depends_kmscube() {
     local depends=(meson ninja-build)
 
     getDepends "${depends[@]}"
+
+    local ver="0.47"
+    if [ -d "$md_build" ]; then
+        ver=$(grep meson_version $md_build/meson.build | cut -d= -f2 | cut -d\' -f1 | xargs)
+    fi
+    if ! hasPackage meson ${ver} ge; then
+        echo "/usr/bin/meson is too old. Using ~/.local/bin/meson"
+        pip3 install --user meson      
+        export PATH=~/.local/bin:$PATH
+    fi
 }
 
 function sources_kmscube() {
+    # test commit: 9f63f359fab1b5d8e862508e4e51c9dfe339ccb0
     gitPullOrClone "$md_build" https://gitlab.freedesktop.org/mesa/kmscube/
 }
 
